9cd2c68e8f02bd05cc69f6a8833bf4f435e53ced
Đặng Đức Tùng <dangductungcfc@gmail.com>
2017-02-20 22:38:15 +0700

    Update index.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,13 @@ var path = process.cwd();
 var body = require('body-parser');
 var urlencoded = body.urlencoded({extended :false});
 var mysql      = require('mysql');
-var connection = mysql.createConnection("mysql://zfdodilksacomvmw:JmJ5cWyk8HVaPK36MGCzHs4uqUfDqAoePX6FbqNgW6JGGzRfgi45wcrqqpNpQ33F@6c704716-6ebc-4d82-a382-a72000f9d811.mysql.sequelizer.com/db6c7047166ebc4d82a382a72000f9d811");
+var connection = mysql.createConnection({
+  host     : '6c704716-6ebc-4d82-a382-a72000f9d811.mysql.sequelizer.com',
+  user     : 'zfdodilksacomvmw',
+  password : 'JmJ5cWyk8HVaPK36MGCzHs4uqUfDqAoePX6FbqNgW6JGGzRfgi45wcrqqpNpQ33F',
+  database : 'db6c7047166ebc4d82a382a72000f9d811'
+
+});
 var conv = new Object();
 var users =new Object();
 module.exports= function(app){
